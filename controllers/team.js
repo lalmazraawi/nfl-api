cbbe3e3bbe3a7480b57b3f67a1021e10935c2cab
Lailah Amazraawi <lailahamazraawi@Lailahs-MacBook-Pro.local>
2022-05-10 20:52:36 -0400

    completed extra credit part two

--- controllers/team.js
+++ controllers/team.js
@@ -15,25 +15,34 @@ const getTeamById = (request, response) => {
 }
 
 const saveNewTeam = (request, response) => {
-  const { id, location, mascot, abbreviation, conference, division } = request.body
+  const {
+    location, mascot, abbreviation, conference, division
+  } = request.body
 
-  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+  if (!location || !mascot || !abbreviation || !conference || !division) {
     // eslint-disable-next-line max-len
-    return response.status(400).send('The following fields are required: id, location, mascot, abbreviation, conference, division')
+    return response.status(400).send('The following fields are required: location, mascot, abbreviation, conference, division')
   }
 
-  const newTeam = { id, location, mascot, abbreviation, conference, division }
+  const id = createId()
+
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
 
   teams.push(newTeam)
 
   return response.status(201).send(newTeam)
 }
+
 const createId = () => {
-    const idArray = teams.map((team) =>{
-        return team.id
+  const idArray = teams.map((team) => {
+    return team.id
+  })
 
-    })
+  const newId = idArray.length + 1
 
+  return newId
 }
 
 module.exports = { getTeamById, getAllTeams, saveNewTeam }
